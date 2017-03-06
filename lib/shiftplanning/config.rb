a87f0d98d3c9af88d3d198fb4f39c7607e9cbd77
Scott Tesler <scottiet123@gmail.com>
2017-03-06 10:02:54 -0600

    Change URI to "humanity"

--- lib/shiftplanning/config.rb
+++ lib/shiftplanning/config.rb
@@ -1,21 +1,21 @@
 =begin
-  
+
   Copyright (C) 2013 Kyle Richardson
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
   as published by the Free Software Foundation; either version 2
   of the License, or (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-   
+
 =end
 
 require 'uri'
@@ -23,16 +23,17 @@ require 'net/http'
 
 class ShiftPlanning
   class Config
-    # Setup class variables 
-    @@uri = URI.parse("http://www.shiftplanning.com/api/")
+    # Setup class variables
+    @@uri = URI.parse("https://www.humanity.com/api/")
     @@http = Net::HTTP.new(@@uri.host, @@uri.port)
+    @@http.use_ssl = @@uri.scheme == 'https'
     @@api_path = @@uri.path
     @@return_types = %w(json xml html)
-    
+
     # Define getters and setters
     attr_accessor :api_key, :token
     attr_reader :output
-    
+
     # Constructor
     #**********************************
     def initialize _api_key, options = {}
@@ -43,12 +44,12 @@ class ShiftPlanning
       self.token = @session[:sp_token] unless @session.nil?
       @@http.set_debug_output(options[:outfile]||$stdout) if options[:verbose]
     end
-    
-    # Setter for output type 
+
+    # Setter for output type
     #**********************************
     def output= _output
       _output = _output.to_s
-        
+
       if @@return_types.include?(_output)
         @output = _output
       else
